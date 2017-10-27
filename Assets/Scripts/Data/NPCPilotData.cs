5c283df1c08f81e35582e1a7abae8d71ecfe03ef
e_m <evanmulrooney@gmail.com>
2017-10-27 11:02:54 -0400

    Issues with git rolling back... nothing to see here!

---
+++ Assets/Scripts/Data/NPCPilotData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCPilotData : MonoBehaviour {
+
+	[Header("Name")]
+	public string firstName = "NPC";
+	public string lastName = "NPC";
+	public string callsign = "N_P_C";
+	public string faction = "Earth";
+	
+
+}

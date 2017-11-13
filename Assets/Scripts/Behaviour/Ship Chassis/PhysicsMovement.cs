2a8f5bb0b2c733de19b6cf7c33a76acdb6e5eb78
e_m <evanmulrooney@gmail.com>
2017-11-13 11:50:30 -0500

    Ships with physics now have a reverse-direction ability

--- Assets/Scripts/Behaviour/Ship Chassis/PhysicsMovement.cs
+++ Assets/Scripts/Behaviour/Ship Chassis/PhysicsMovement.cs
@@ -14,6 +14,11 @@ public class PhysicsMovement : ShipChassis {
 	Rigidbody2D rb2d;
 	public Vector3 reverseVelocity;
 
+	bool inputSinceBrakes = false;
+
+	float targetAngle;
+	float rotationTolerance = 5f;
+
 	public override void Awake() {
 		rb2d = GetComponent<Rigidbody2D>();
 
@@ -29,10 +34,42 @@ public class PhysicsMovement : ShipChassis {
 	public override void ShipThrust(float acceleration) {
 		currentThrust += acceleration;
 		rb2d.AddForce(transform.up * currentThrust);
+		ShipInput();
 	}
 
+	public override void ShipRotate(float horizontalInput) {
+		base.ShipRotate(horizontalInput);
+		ShipInput();
+	}
+
+	void BrakesRotate(float falseHorizontalInput) {
+		base.ShipRotate(falseHorizontalInput);
+	}
+
+
+	void ShipInput() {
+		if (!inputSinceBrakes) {
+			inputSinceBrakes = true;
+		}
+	}
+
+
 	public override void ShipBrakes(float deacceleration) {
-		//
+		Debug.Log(horizontalInput);
+		if (inputSinceBrakes) {
+			float longVelocity = Mathf.Abs(rb2d.velocity.x) > Mathf.Abs(rb2d.velocity.y) ? rb2d.velocity.x : rb2d.velocity.y;
+			float shortVelocity = Mathf.Abs(rb2d.velocity.x) < Mathf.Abs(rb2d.velocity.y) ? rb2d.velocity.x : rb2d.velocity.y;
+			targetAngle = Mathf.Atan2(shortVelocity, longVelocity) * Mathf.Rad2Deg;
+			targetAngle = Mathf.Repeat(transform.eulerAngles.z - 180, 360);
+			Debug.Log(targetAngle + " <----TARGET ANGLE");
+			inputSinceBrakes = false;
+		} else {
+			if (transform.eulerAngles.z > targetAngle + rotationTolerance) {
+				BrakesRotate(-1f);
+			} else if (transform.eulerAngles.z < targetAngle - rotationTolerance) {
+				BrakesRotate(1f);
+			}
+		}
 	}
 
 }

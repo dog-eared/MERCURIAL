8b745b3ffb4a9df13ac39cbc26c4ba44ac894677
e_m <evanmulrooney@gmail.com>
2017-11-13 13:43:20 -0500

    Reverse direction polished and (slightly) refactored!

--- Assets/Scripts/Behaviour/Ship Chassis/PhysicsMovement.cs
+++ Assets/Scripts/Behaviour/Ship Chassis/PhysicsMovement.cs
@@ -14,10 +14,14 @@ public class PhysicsMovement : ShipChassis {
 	Rigidbody2D rb2d;
 	public Vector3 reverseVelocity;
 
-	bool inputSinceBrakes = false;
+	bool movementSinceBrakes = false; //NOTE: ShipData may need to set this to "true"
+																		//when the player is hit if we want them to be able
+																		//to cleanly reverse out of weapon impacts that have
+																		//substantial physics.
 
 	float targetAngle;
 	float rotationTolerance = 5f;
+
 
 	public override void Awake() {
 		rb2d = GetComponent<Rigidbody2D>();
@@ -26,10 +30,12 @@ public class PhysicsMovement : ShipChassis {
 
 	}
 
+
 	public override void ShipMovement() {
 		rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maximumSpeed);
 
 		}
+
 
 	public override void ShipThrust(float acceleration) {
 		currentThrust += acceleration;
@@ -37,37 +43,46 @@ public class PhysicsMovement : ShipChassis {
 		ShipInput();
 	}
 
+
 	public override void ShipRotate(float horizontalInput) {
 		base.ShipRotate(horizontalInput);
 		ShipInput();
 	}
 
+
 	void BrakesRotate(float falseHorizontalInput) {
+		//Function for rotating the ship without setting movementSinceBrakes flag
+		//This lets us control when the targetAngle is set by hitting brakes.
 		base.ShipRotate(falseHorizontalInput);
 	}
 
 
 	void ShipInput() {
-		if (!inputSinceBrakes) {
-			inputSinceBrakes = true;
+		if (!movementSinceBrakes) {
+			movementSinceBrakes = true;
 		}
 	}
 
 
 	public override void ShipBrakes(float deacceleration) {
-		Debug.Log(horizontalInput);
-		if (inputSinceBrakes) {
-			float longVelocity = Mathf.Abs(rb2d.velocity.x) > Mathf.Abs(rb2d.velocity.y) ? rb2d.velocity.x : rb2d.velocity.y;
-			float shortVelocity = Mathf.Abs(rb2d.velocity.x) < Mathf.Abs(rb2d.velocity.y) ? rb2d.velocity.x : rb2d.velocity.y;
-			targetAngle = Mathf.Atan2(shortVelocity, longVelocity) * Mathf.Rad2Deg;
-			targetAngle = Mathf.Repeat(transform.eulerAngles.z - 180, 360);
-			Debug.Log(targetAngle + " <----TARGET ANGLE");
-			inputSinceBrakes = false;
+		if (movementSinceBrakes) {
+
+			float tanOpposite = rb2d.velocity.y;
+			float tanAdjacent = rb2d.velocity.x;
+
+			targetAngle = Mathf.Atan2(tanOpposite, tanAdjacent) * Mathf.Rad2Deg;
+
+			targetAngle = Mathf.Repeat(targetAngle + 90, 360);
+
+			movementSinceBrakes = false;
+
 		} else {
+
 			if (transform.eulerAngles.z > targetAngle + rotationTolerance) {
 				BrakesRotate(-1f);
 			} else if (transform.eulerAngles.z < targetAngle - rotationTolerance) {
 				BrakesRotate(1f);
+
 			}
 		}
 	}
